96d05cc85aba0b3707c905d4babaaf23549036ce
Test <zhli@pku.edu.cn>
2021-12-02 20:42:45 +0800

    Add files via upload

---
+++ Gate2/gate2.C
@@ -0,0 +1,643 @@
+
+#include "TH1.h"
+#include "TH2.h"
+#include "TFile.h"
+#include "TCanvas.h"
+#include "TList.h"
+#include "TString.h"
+#include "TSpectrum.h"
+#include "TLatex.h"
+#include "TColor.h"
+#include "TPolyMarker.h"
+#include "TApplication.h"
+#include "TROOT.h"
+#include <map>
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#define codename         "Gamma-Gamma two-fold matrix data analysis Code GATE2"
+#define author           "by Li Zhihuan,  Peking University"
+#define lastmodified     "last modified on Dec. 30, 2021"
+#define webpage          "https://github.com/zhihuanli/gamma-gamma-coincidence-analysis/tree/master/prompt%20gg"
+
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */                                                                
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+
+struct parameters
+{
+  int ncanvas;
+  int xmin;    //range for hist
+  int xmax;    //range for hist
+  int npeaks;  //number of peaks for TSpectrum
+  float dge1;    //gate width:ge+dge1 - ge+dge2
+  float dge2;
+  bool peak_background;//true: all negative counts will be set to 0 
+};
+parameters st;
+
+map<TString,int> mg;
+map<TString,TString> mgtitle;
+map<TString,int>::iterator img;
+map<TString,TString>::iterator imgtitle;
+
+TList *tlhist,*tlcanv;
+
+int xmin=0;
+int xmax;
+
+TFile *f;
+TH1 *hg2x;
+TH2 *hg2xy;
+
+TCanvas *ca[1000];
+int ic=-1;//canvas id
+int icy=1;
+int ncy=1;//number of windows in y axia
+int ih=0;
+
+void setncanvas(int ncanvas=5) { st.ncanvas=ncanvas; }
+void setxrange(int xmin1=xmin, int xmax1=xmax){ st.xmin=xmin1; st.xmax=xmax1;};
+void setnpeaks(int npeaks1=30) {st.npeaks=npeaks1;};
+void setpeakwidth(float dgea=-2,float dgeb=2) {st.dge1=dgea; st.dge2=dgeb;};
+void setpeakbackground(bool pb=false) {st.peak_background=pb;};
+void showSettings();
+// Show different parts of a gated-spectrum in the same window
+void gs(float ge1,int npad=4);
+//gated-spectra up to six peaks - draw existing histograms
+void gm(float ge1=0,float ge2=0,float ge3=0,float ge4=0,float ge5=0,float ge6=0);
+void gm(TH1* h1, TH1 *h2=NULL, TH1 *h3=NULL, TH1 *h4=NULL,TH1 *h5=NULL, TH1 *h6=NULL);
+
+//total projection sepctra
+void tpjm(int npad=4);
+
+//two-fold gated with a specified gamma peak range
+TString gw(float ge1=-1,float ge2=-1);
+//and
+TString gand(float ge1=0,float ge2=0,float ge3=0,float ge4=0,float ge5=0,float ge6=0);
+//add 
+TString gadd(float ge1=0,float ge2=0,float ge3=0,float ge4=0,float ge5=0,float ge6=0);
+//sub
+TString gsub(float ge1=0,float ge2=0,float ge3=0,float ge4=0,float ge5=0,float ge6=0);
+//help
+void help();
+
+
+//show all existing gated histograms
+void histlist(){
+  cout<<GREEN<<endl;
+  tlhist->ls();
+  cout<<RESET<<endl;
+ };//show name of all histograms
+
+//show all existing TCanvas
+void canvlist(){
+  cout<<GREEN<<endl;
+  tlcanv->ls();
+  cout<<RESET<<endl;
+ };
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+//private functions
+//two-fold gated
+TString g(double ge=0,int icy1=1);
+void newcanvas(int ncy,TString sctitle);
+TString GetHistName(TString sprefix);
+void tpj(int icy=1);
+//mark peak positions
+TString peaks(TH1 *h, Double_t thres=0.05);
+
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+void gate2(TString fname="hggmatrix.root",TString sg2="hg2xyp")
+{
+  TColor::SetColorThreshold(0.1);
+  cout<<endl;
+  cout<< CYAN<<codename <<endl;
+  cout<< YELLOW<<author <<endl;
+  cout<< YELLOW<<lastmodified <<endl;
+  cout<< YELLOW<<webpage <<endl;
+  cout<< RESET<<endl;
+  if(f==NULL) {
+    f=new TFile(fname);
+    cout<<GREEN<<"ROOT file: "<<fname<<RESET<<endl;
+  }
+
+#if ROOT_VERSION_CODE < ROOT_VERSION(6,0,0)
+  cout<<"This code is valid only for  ROOT6 !"<<endl;
+  gApplication->Terminate(0);
+#endif
+
+  //two-fold
+  // hg2xy=(TH2*)f->Get(sg2.Data());
+
+  
+  if(!(hg2xy=(TH2*)f->Get(sg2.Data()))) {
+    cout<<RED<<"ERROR!  No 2D-histogram in "<<fname<<" is detected !"<<RESET<<endl;
+     gApplication->Terminate(0);
+  }
+  else cout<<GREEN<<"2D-histogram: "<<sg2.Data()<<endl;
+  hg2x=(TH1*) hg2xy->ProjectionX("hg2x");
+  
+  xmax=hg2x->GetBinCenter(hg2x->GetXaxis()->GetNbins());
+  cout<<GREEN<<Form("Range of X/Y: %d - %d ",xmin,xmax)<<RESET<<endl<<endl;
+
+  
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+  setncanvas(5);
+  setnpeaks(30);
+  setpeakwidth(-3,3);
+  setpeakbackground(0);//
+
+  showSettings();
+  cout<<RED<<"help() for short instructions "<<RESET<<endl<<endl;
+  
+  if(tlhist==NULL) tlhist=new TList();
+  if(tlcanv==NULL) tlcanv=new TList();  
+  tpjm();
+  // if(aae!=NULL) gaem();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void tpjm(int npad)
+{
+    if(npad>6) npad=6;
+  newcanvas(npad,"Total Projection Spectrum");
+  int xmina=st.xmin;
+  int xmaxa=st.xmax;
+  int dx=(xmax-xmin)/npad;
+  for(int i=0;i<npad;i++) {
+    setxrange(xmin+i*dx,xmin+(i+1)*dx);
+    tpj(i+1);
+  }
+  setxrange(xmina,xmaxa);
+  
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+void gs(float ge1,int npad)
+{
+  if(npad>6) npad=6;
+  double ge[6]={ge1};
+  TString sctitle=Form("gs(%.1f)",ge[0]);
+  newcanvas(npad,sctitle);
+  int xmina=st.xmin;
+  int xmaxa=st.xmax;
+  int dx=(xmax-xmin)/npad;
+  for(int i=0;i<npad;i++) {
+    setxrange(xmin+i*dx,xmin+(i+1)*dx);
+    g(ge[i],i+1);
+  }
+  setxrange(xmina,xmaxa);
+}
+
+TString gw(float ge1,float ge2)
+{
+  if(ge1==-1 || ge2==-1) {
+    cout<<"At least two gates should be specified !"<<endl;
+    return "error";
+  }
+  if(ge1<xmin) ge1=xmin;
+  if(ge2>xmax) ge2=xmax;
+  TString sctitle=Form("gw(%.1f,%.1f)",ge1,ge2);  
+  newcanvas(1,sctitle);
+  TString stitle=Form("Gated on %.1f-%.1f keV",ge1,ge2);
+  int gea=hg2xy->GetXaxis()->FindBin(ge1);
+  int geb=hg2xy->GetXaxis()->FindBin(ge2);
+
+  TString sname=GetHistName(Form("gw%d-%d",int(ge1),int(ge2)));
+  TH1F *ha=(TH1F*)hg2xy->ProjectionX(sname.Data(),gea,geb);
+  ha->SetTitle(stitle);
+  if(!tlhist->FindObject(ha)) tlhist->Add(ha);
+  return peaks(ha);
+}
+void gm(float ge1,float ge2,float ge3,float ge4, float ge5, float ge6)
+{
+  int npad=0;
+  double ge[6]={ge1,ge2,ge3,ge4,ge5,ge6};
+  for(int i=0;i<6;i++) 
+    if(ge[i]>1) npad++;
+  TString sctitle=Form("gm(%.1f",ge[0]);
+  for(int i=1;i<6;i++) 
+    if(ge[i]>0) sctitle+=Form(",%.1f",ge[i]);
+  sctitle+=")";
+  newcanvas(npad,sctitle);
+  for(int i=0;i<npad;i++)
+    g(ge[i],i+1);
+}
+
+void gm(TH1* h1, TH1 *h2, TH1 *h3, TH1 *h4, TH1 *h5, TH1 *h6)
+{
+  int npad=0;
+  TH1 *hg[6]={h1,h2,h3,h4,h5,h6};
+  for(int i=0;i<6;i++) 
+    if(hg[i]!=NULL) npad++;
+  TString sctitle=Form("gm(%s",hg[0]->GetName());
+  for(int i=1;i<6;i++) 
+    if(hg[i]!=NULL) sctitle+=Form(",%s",hg[i]->GetName());
+  sctitle+=")";
+  newcanvas(npad,sctitle);
+  for(int i=0;i<npad;i++) {
+    ca[ic]->cd(i+1);
+    peaks(hg[i]);
+  }
+}
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+TString gand(float ge1, float ge2, float ge3, float ge4,float ge5,float ge6)
+{
+  if(ge1==0 || ge2==0) {
+    cout<<"At least two gates should be specified !"<<endl;
+    return "error";
+  }
+  int npad=0;
+  TH1F *ha[6];
+  double ge[6]={ge1,ge2,ge3,ge4,ge5,ge6};
+  for(int i=0;i<6;i++) 
+    if(ge[i]>1) npad++;
+  
+  TString sctitle=Form("gand(%.1f",ge[0]);
+  for(int i=1;i<6;i++) 
+    if(ge[i]>0) sctitle+=Form(",%.1f",ge[i]);
+  sctitle+=")";
+  
+  newcanvas(npad+1,sctitle);
+
+  TString stitle="And gates of";
+  for(int i=0;i<npad;i++) {
+    TString hname=g(ge[i],i+1);//return name of histogram.
+    ha[i]=(TH1F*)gROOT->FindObject(hname);
+    stitle=Form("%s %.1f",stitle.Data(),ge[i]);
+  }
+  stitle=Form("%s keV",stitle.Data());
+  TString sname=GetHistName("gand");  
+  TH1F *hand=(TH1F*)hg2xy->ProjectionX(sname.Data(),xmin,xmax);//copy hist structure
+  hand->SetTitle(stitle);
+  hand->Reset("M");//!!! reset maximum and minimum;
+  for(int i=1;i<=ha[0]->GetNbinsX();i++){
+    double ma=ha[0]->GetBinContent(i);
+    for(int j=1;j<npad;j++) {
+      double mb=ha[j]->GetBinContent(i);
+      ma=TMath::Min(ma,mb);
+    }
+    hand->SetBinContent(i,ma);
+  }
+  if(!tlhist->FindObject(hand)) tlhist->Add(hand);
+ 
+  ca[ic]->cd(npad+1);
+  icy=npad+1;
+  return peaks(hand);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+TString gadd(float ge1, float ge2, float ge3, float ge4, float ge5, float ge6)
+{
+  if(ge1==0 || ge2==0) {
+    cout<<"At least two gates should be specified !"<<endl;
+    return "error";
+  }
+  int npad=0;
+  TH1F *ha[6];
+  double ge[6]={ge1,ge2,ge3,ge4,ge5,ge6};
+  for(int i=0;i<6;i++) 
+    if(ge[i]>1) npad++;
+  
+  TString sctitle=Form("gadd(%.1f",ge[0]);
+  for(int i=1;i<6;i++) 
+    if(ge[i]>0) sctitle+=Form(",%.1f",ge[i]);
+  sctitle+=")";
+  newcanvas(npad+1,sctitle);
+  TString stitle="Add gates of";
+  
+  for(int i=0;i<npad;i++) {
+    TString hname=g(ge[i],i+1);//return name of histogram.
+    ha[i]=(TH1F*)gROOT->FindObject(hname);
+    stitle=Form("%s %.1f",stitle.Data(),ge[i]);
+  }
+  stitle=Form("%s keV",stitle.Data());
+  TString sname=GetHistName("gadd");    
+  TH1F *hadd=(TH1F*)hg2xy->ProjectionX(sname.Data(),xmin,xmax);//copy hist structure
+  hadd->SetTitle(stitle);
+  hadd->Reset("M");//!!! reset maximum and minimum;
+
+  for(int i=0;i<npad;i++) hadd->Add(hadd,ha[i]);
+   if(!tlhist->FindObject(hadd))tlhist->Add(hadd);
+  ca[ic]->cd(npad+1);
+  icy=npad+1;
+  return peaks(hadd);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+TString gsub(float ge1, float ge2, float ge3, float ge4, float ge5, float ge6)
+{
+
+  if(ge1==0 || ge2==0) {
+    cout<<"At least two gates should be specified !"<<endl;
+    return "error";
+  }
+  int npad=0;
+  TH1F *ha[6];
+  double ge[6]={ge1,ge2,ge3,ge4,ge5,ge6};
+  for(int i=0;i<6;i++) 
+    if(ge[i]>1) npad++;
+  
+  TString sctitle=Form("gsub(%.1f",ge[0]);
+  for(int i=1;i<6;i++) 
+    if(ge[i]>0) sctitle+=Form(",%.1f",ge[i]);
+  sctitle+=")";
+  
+  newcanvas(npad+1,sctitle);
+  TString stitle="Sub gates of";
+  for(int i=0;i<npad;i++) {
+    TString hname=g(ge[i],i+1);//return name of histogram.
+    ha[i]=(TH1F*)gROOT->FindObject(hname);
+    stitle=Form("%s %.1f",stitle.Data(),ge[i]);
+  }
+  stitle=Form("%s keV",stitle.Data());
+  TString sname=GetHistName("gsub");    
+  TH1F *hsub=(TH1F*)hg2xy->ProjectionX(sname.Data(),xmin,xmax);//copy hist structure
+
+  hsub->SetTitle(stitle);
+  hsub->Reset("M");//!!! reset maximum and minimum;
+
+  for(int i=1;i<npad;i++) hsub->Add(ha[0],ha[i],1,-1);
+  if(!tlhist->FindObject(hsub)) tlhist->Add(hsub);
+  ca[ic]->cd(npad+1);
+  icy=npad+1;
+  return peaks(hsub);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+
+
+void saveHist(TH1* hist, TString sfile)
+{
+  TFile *fout=new TFile(sfile.Data(),"UPDATE");
+  hist->Write();
+  fout->Close();
+  cout<<"Write "<<hist->GetName()<<" to "<<sfile.Data()<<endl;
+}
+
+void saveHistAll(TString sfile)
+{
+  TFile *fout=new TFile(sfile.Data(),"UPDATE");
+  tlhist->Write();
+  fout->Close();
+  cout<<"Write all existing histograms to "<<sfile.Data()<<endl;  
+}
+
+void saveHistData(TH1* hist, TString sfile)
+{
+  ofstream fout(sfile.Data());
+  for(int i=1;i<=hist->GetNbinsX();i++) {
+    fout<<Form("%.1f   %.1f",hist->GetBinCenter(i),hist->GetBinContent(i))<<endl;
+  }
+  fout.close();
+  cout<<"Write "<<hist->GetName()<<" to "<<sfile.Data()<<endl;
+}
+
+void saveCanv(TCanvas* c1, TString sfile)
+{
+  TFile *fout=new TFile(sfile.Data(),"UPDATE");
+  c1->Write();
+  fout->Close();
+  cout<<"Write "<<c1->GetName()<<" to "<<sfile.Data()<<endl;
+}
+
+void saveCanvAll(TString sfile)
+{
+  TFile *fout=new TFile(sfile.Data(),"UPDATE");
+  TIter next(tlcanv);
+  while (TCanvas* cc=(TCanvas*)next()) cc->Write();
+  //tlcanv->Write();//some error messages when browse through TCanvas in the root file in MacOS.
+  fout->Close();
+  cout<<"Write all existing histograms to "<<sfile.Data()<<endl;  
+}
+
+void showSettings()
+{
+  cout<<RED<<"Current parameter settings:"<<endl;
+  cout<<CYAN<<"ncanvas = "<<st.ncanvas<<endl;
+  cout<<CYAN<<"xmin = "<<st.xmin<<", xmax= "<<st.xmax<<endl;
+  cout<<CYAN<<"npeaks = "<<st.npeaks<<endl;
+  cout<<CYAN<<"dge1 = "<<st.dge1<<", dge2= "<<st.dge2<<endl;
+  cout<<RESET<<endl;
+}
+
+
+void quit()
+{
+  gApplication->Terminate(0);
+}
+
+void help()
+{
+  cout<<YELLOW<<"....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......"<<endl;
+  cout<< RED<<"Setup, "<<RED<<"changes will take effect for the next drawing."<<endl;
+  cout<< CYAN<< "   setxrange(int xmin1, int xmax1)        - Change range of x-axis."<<endl;
+  cout<< "   setnpeaks(int npeaks1=30)              - Change the number of peaks marked."<<endl;
+  cout<< "   setpeakwidth( int dgea=-2, int dgeb=2) - Change range of gate to peak-dgea to peak+dgea."<<endl;
+  cout<< "   showSettings()                         - Show current parameter settings."<<endl;
+  
+  cout<<YELLOW<<"....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......"<<endl;
+  cout<<RED<<"Show different parts of a gated-spectrum in the same window."<<endl;
+  cout<<CYAN<<"   gs(float ge1,int npad=4)"<<endl;
+  cout<<RED<<"Gated-spectra up to six peaks/histograms"<<endl;
+  cout<<CYAN<<  "   gm(double ge1, [double ge2], ...)"<<endl;
+  cout<<  "   gm(TH1* h1, [TH1* h2], ...)"<<endl;
+  cout<<RED<< "Total projection sepctrum"<<endl;
+  cout<<CYAN<< "   tpjm(int left=0, int middle=xmax/2., int right=xmax)"<<endl;
+  cout<<RED<< "Gamma spectrum gated from ge1 to ge2"<<endl;
+  cout<<CYAN<< "   gw(float ge1,float ge2)"<<endl;
+  cout<<RED<< "AND, ADD, SUB gated spectra up to six spectra"<<endl;
+  cout<<CYAN<<"   gand(float ge1,float ge2, [float ge3], ...)"<<endl;
+  cout<<"   gadd(float ge1,float ge2, [float ge3], ...)"<<endl;
+  cout<<"   gsub(float ge1,float ge2, [float ge3], ...)"<<endl;
+
+
+  cout<<YELLOW<<"....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......"<<endl;
+  cout<<RED<<"Save a hist to a ROOT file with append mode"<<endl;
+  cout<<CYAN<<"    saveHist(TH1* hist, TString sfile)"<<endl;
+  cout<<RED<<"Save a canvas to a ROOT file with append mode"<<endl;
+  cout<<CYAN<<"    saveCanv(TCanvas* c1, TString sfile)"<<endl;
+  cout<<RED<<"Save a hist data to a text file"<<endl;
+  cout<<CYAN<<"    saveHistData(TH1* c1, TString sfile)"<<endl;
+  cout<<RED<<"Save All histograms to a ROOT file"<<endl;
+  cout<<CYAN<<"    saveHistAll(TString sfile)"<<endl;
+    cout<<RED<<"Save All TCanvas to a ROOT file"<<endl;
+  cout<<CYAN<<"    saveCanvAll(TString sfile)"<<endl;
+  
+  cout<<RED<<"Show all existing gated histograms"<<endl;
+  cout<<CYAN<<"   histlist()"<<endl;
+
+    cout<<RED<<"Show all existing TCanvas"<<endl;
+  cout<<CYAN<<"   canvlist()"<<endl;
+    
+  cout<<RESET<<endl;
+}
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+//private functions 
+void newcanvas(int ncy1,TString sctitle)
+{
+  ic++;
+  ic=ic%st.ncanvas;
+  ncy=ncy1;
+  double w=800;
+  double h=800;
+  if(ncy1==1)h=400;
+  sctitle=Form("%s_%d",sctitle.Data(),ic);
+  if(ca[ic]) {
+    tlcanv->Remove(ca[ic]);
+    delete ca[ic];
+  }
+  ca[ic]=new TCanvas(Form("canv%i",ic),sctitle.Data(),w,h);
+
+  ca[ic]->Divide(1,ncy);
+  for(int i=1;i<=ncy;i++) {
+    ca[ic]->GetPad(i)->SetBottomMargin(0.05);
+    ca[ic]->GetPad(i)->SetTopMargin(0.005);
+    ca[ic]->GetPad(i)->SetLeftMargin(0.08);
+    ca[ic]->GetPad(i)->SetRightMargin(0.05);
+  }
+  ca[ic]->ToggleEventStatus();
+  tlcanv->Add(ca[ic]);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+//generate histogram name according to prefix and title 
+TString GetHistName(TString sprefix)
+{
+
+  img=mg.find(sprefix);
+  int gid;
+  if(img!=mg.end()){
+      img->second++; 
+  }
+  else {
+    gid=0;
+    mg.insert(make_pair(sprefix,gid));
+    img=mg.find(sprefix);
+  }
+  return Form("%s_%d",(img->first).Data(),img->second);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void tpj(int icy)
+{
+  if(icy>ncy) icy=ncy;
+  ca[ic]->cd(icy);
+  TString stitle=Form("Total Projection Sepctrum (%d - %d) keV ",st.xmin,st.xmax);
+  TString sname=GetHistName("gtpj");   
+  TH1F *h=(TH1F*)hg2xy->ProjectionX(sname.Data(),xmin,xmax);
+  h->SetTitle(stitle.Data());
+  if(!tlhist->FindObject(h)) tlhist->Add(h);
+  peaks(h);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+    
+//two fold - gated on x axis
+TString g(double ge, int icy1)
+{
+  if(ge==0) {
+    cout<<"wrong parameters!"<<endl;
+    return "error";
+  }
+  if(icy1>ncy) icy=ncy;
+  else icy=icy1;
+  ca[ic]->cd(icy);
+
+  int gea=hg2xy->GetXaxis()->FindBin(ge+st.dge1);
+  int geb=hg2xy->GetXaxis()->FindBin(ge+st.dge2);
+  if(gea<xmin) gea=xmin;
+  if(geb>xmax) geb=xmax;
+  TString stitle=Form("Gated on %.1f keV (%.1f - %.1f)",ge,ge+st.dge1,ge+st.dge2);
+  
+  TString sname=GetHistName(Form("g%d",int(ge)));
+ 
+  TH1F *ha=(TH1F*)hg2xy->ProjectionX(sname.Data(),gea,geb);
+  ha->SetTitle(stitle);
+  if(!tlhist->FindObject(ha)) tlhist->Add(ha);
+  return peaks(ha);
+  
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+TString peaks(TH1 *h, Double_t thres)
+{
+  double x0=h->GetBinLowEdge(h->GetNbinsX());
+  double x1=h->GetBinLowEdge(0);
+  if(st.xmin<x1) st.xmin=x1;
+  if(st.xmax>x0) st.xmax=x0;
+  h->SetAxisRange(st.xmin,st.xmax,"X");
+  double ymin=h->GetBinContent(h->GetMinimumBin());
+  double ymax=h->GetBinContent(h->GetMaximumBin())*1.3;
+  if(ymin<0 && ymin<-0.1*ymax) ymin=-0.1*ymax;//ymin=0.01;
+  cout<< CYAN << ic<<" "<<icy<<" "<<h->GetName()<<"  ["<<h->GetTitle()<<"]"<<RESET<<endl;
+  h->GetYaxis()->SetNoExponent();
+  h->SetLineColor(kBlue);
+  h->SetFillColorAlpha(kCyan,0.15);
+  TSpectrum *s=new TSpectrum(500);
+  /*
+  if(st.peak_background) {
+    for(int i=1;i<=h->GetNbinsX();i++) //set bins with negative counts to zero
+       if(h->GetBinContent(i)<0)  h->SetBinContent(i,0);
+      TH1F *hb=(TH1F*)s->Background(h,5,"same");
+      h->Add(h,hb,1,-1);
+      }*/
+    h->Sumw2(0);
+  h->SetAxisRange(ymin,ymax,"Y");
+  h->SetStats(0);
+  Int_t nfound=100;
+  Int_t nloop=0;
+   while(nloop<50){
+    nfound=s->Search(h,1.5,"",thres);
+    if(nfound>st.npeaks) thres += 0.005;
+    else thres -= 0.005;
+    if(thres<0 || abs(nfound-st.npeaks)<3 ) break;
+    nloop++;
+    }
+  TPolyMarker *pm=(TPolyMarker *)
+                      h->GetListOfFunctions()->FindObject("TPolyMarker");
+  pm->SetMarkerStyle(2);//32
+  pm->SetMarkerColor(kRed);
+  pm->SetMarkerSize(0.5);//0.4
+  Double_t *xpeaks=s->GetPositionX();
+  Double_t *ypeaks=s->GetPositionY();
+  for(int j=0;j<nfound;j++) {
+   stringstream ss;
+   ss<<xpeaks[j];
+   if(ypeaks[j]<3) continue;
+   TString s1=ss.str();
+   TLatex *tex=new TLatex(xpeaks[j],ypeaks[j]+ymax*0.02,s1);
+   
+   tex->SetTextFont(133);//13
+   tex->SetTextSize(16);
+   tex->SetTextAlign(12);
+   tex->SetTextAngle(90);
+   tex->SetTextColor(kRed);
+   tex->Draw();
+   h->GetListOfFunctions()->Add(tex);
+  }
+  ca[ic]->Draw();
+  return h->GetName();
+}
+ 
