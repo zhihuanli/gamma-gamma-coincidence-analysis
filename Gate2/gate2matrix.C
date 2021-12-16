f1cce811ebb287478b38c9136e2fa4a89c83b465
Test <zhli@pku.edu.cn>
2021-12-16 19:49:17 +0800

    Add files via upload

---
+++ Gate2/gate2matrix.C
@@ -0,0 +1,109 @@
+// Generate background spectrum and gg-matrix using Radware approch.
+// Author: Li Zhihuan, Peking University
+
+//> root -l gate2matrix.C
+//root[1] genBackground() //generate background spectrum
+//root[2] gate2mat() // gg-matrix
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TString.h"
+#include "TROOT.h"
+#include "TSpectrum.h"
+#include <iostream>
+#include <ctime>
+#define RESET   "\033[0m"
+#define RED     "\033[31m"      /* Red */
+using namespace std;
+
+//input 
+TString fInName = "g2hist.root";//root file 
+TString h2name = "h2xy";//gg-matrix 
+//output 
+TString fOutName = "gate2matrix.root";//root file
+
+////////////////////////////////////////////////////////
+TH2F *hg2xy;
+int nbin; 
+float xmin;
+float xmax;
+
+//1d total projection
+TH1F *hg2x;
+
+//matrix
+TH2F *hg2xyb;
+TH2F *hg2xyp;
+
+TH1F *hg2xb, *hg2xp;
+void genBackground(int res=20);
+void gate2mat();
+
+void gate2matrix()
+{
+  TFile *fin = new TFile(fInName.Data());
+  if ((!fin) || (fin->IsZombie())) {
+    cout<<fInName.Data()<<" does not exist!"<<endl;
+    return;
+  }
+  hg2xy = (TH2F*)fin->Get(h2name.Data()); //original gamma-gamma matrix
+  if(!hg2xy) return;
+  nbin = hg2xy->GetNbinsX();
+  xmin = hg2xy->GetXaxis()->GetBinLowEdge(1);
+  xmax = hg2xy->GetXaxis()->GetBinLowEdge(nbin+1);
+  //1d total projection
+  hg2x = (TH1F*)hg2xy->ProjectionX("hg2x",1,nbin);//total projection spectrum
+  cout<<nbin<<","<<xmin<<","<<xmax<<endl;
+  
+  //matrix
+  hg2xyb = new TH2F("hg2xyb","bg matrix",nbin,xmin,xmax,nbin,xmin,xmax);//background matrix
+  hg2xyp = new TH2F("hg2xyp","bg subtracted matrix",nbin,xmin,xmax,nbin,xmin,xmax);//background subtracted matrix
+  genBackground();
+
+}
+void genBackground(int res)
+{
+  if(hg2xb) hg2xb->Clear();
+  if(hg2xp) hg2xp->Clear();
+  TSpectrum *sa = new TSpectrum(1000);
+  hg2xb=(TH1F*)sa->Background(hg2x,res,"nosmoothing");//background spectrum, user
+  hg2xp=new TH1F("hg2xp","",nbin,xmin,xmax);//peak spectrum
+  hg2x->Draw();
+  hg2xb->Draw("same");
+  hg2xp->Add(hg2x,hg2xb,1,-1); 
+  cout<<endl;
+cout<<RED<<"(1).If the shape of the background spectrum looks OK, then run gate2matrix() "<<endl;
+cout<<"(2).If not then run genBackground(res) with modified res value until the (1) is meet." <<RESET<<endl;
+}
+
+void gate2mat()
+{
+  //make background matrix - RADWARE approch
+  Double_t T,Pi,Pj,pi,pj,Bij;
+  T=hg2x->Integral();
+  int N=hg2x->GetNbinsX();
+  for(int i=1; i<=N; i++) {
+    for(int j=1; j<=N; j++) {
+      Pi=hg2x->GetBinContent(i);
+      Pj=hg2x->GetBinContent(j);
+      pi=hg2xp->GetBinContent(i);
+      pj=hg2xp->GetBinContent(j);
+      Bij=(Pi*Pj-pi*pj)/T;
+      Double_t x=hg2x->GetBinCenter(i);
+      Double_t y=hg2x->GetBinCenter(j);       
+      hg2xyb->Fill(x,y,Bij);
+    }
+  }
+  hg2xyp->Sumw2(0);
+  hg2xyp->Add(hg2xy,hg2xyb,1,-1);
+
+  //output to ROOT file
+  TFile *fout=new TFile(fOutName.Data(),"RECREATE");//user
+  hg2xyp->Write();
+  hg2x->Write();
+  hg2xb->Write();
+  fout->Close();
+  cout<<"Write Histgrams hg2xyp, hg2x and hg2xb to "<<fOutName.Data()<<endl;
+}
---
+++ Gate2/r2g2hist.C
@@ -0,0 +1,87 @@
+// Make 2-fold gg-matrix(TH2) from ROOT files(TTree) 
+// Author: Li Zhihuan, Peking University
+// > root -l r2g2hist.C
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TString.h"
+#include "TROOT.h"
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+//range of runid
+int RunIdStart = 5;
+int RunIdStop = 78;
+//input
+TString fNameFormat = "./data/test%04d.root";//Path+filename, %04d: xxx0005.root for runid=5;
+TString treeName="t";
+//gamma Tree Branches in the input file
+TString ghitBranch="nGammaOneEventAddBack";//hit
+TString geBranch="nGammaEnergyAddBack";//ge[hit]
+TString gtBranch="nGammaTimeAddBack";//gt[hit]
+
+float dgt=18;//coincidence time window.
+//output
+int nBin = 4000;
+int MaxE = 4000;
+TString fOutName="g2hist.root";
+TH1I *h2x=new TH1I("h2x","h2x",nBin,0,MaxE);
+TH2I *h2xy=new TH2I("h2xy","h2xy",nBin,0,MaxE,nBin,0,MaxE);
+
+int root2g2hist(int run);
+
+void r2g2hist()
+{
+    clock_t start,end;
+    start = clock();
+
+    for(int i=RunIdStart;i<=RunIdStop;i++)
+        root2g2hist(i);//tree to TH
+
+    TFile *fout = new TFile(fOutName.Data(),"recreate");//output file 
+    h2x->Write();
+    h2xy->Write();
+    fout->Close();
+
+    end=clock();
+    double seconds=(double)(end-start)/CLOCKS_PER_SEC;
+    cout<<"Time taken by program is : "<<seconds<<" seconds."<<endl;
+}
+
+int root2g2hist(int run)
+{
+  if(gSystem->AccessPathName(Form(fNameFormat.Data(),run))) 
+    {
+      cout <<Form(fNameFormat.Data(),run)<< " does not exist!" <<endl;
+      return 0;
+    }
+    TFile *f=new TFile(Form(fNameFormat.Data(),run)); 
+    TTree *tree=(TTree*) f->Get(treeName.Data());
+    int ghit;
+    double ge[1000];
+    Long64_t gt[1000];
+    tree->SetBranchAddress(ghitBranch.Data(),&ghit);//hit
+    tree->SetBranchAddress(geBranch.Data(),&ge);//energy
+    tree->SetBranchAddress(gtBranch.Data(),&gt);//time    
+    Long64_t nentries=tree->GetEntries();
+    cout<<run<<","<<nentries<<endl;
+    for(Long64_t jentry=0;jentry<nentries;jentry++)  {
+        tree->GetEntry(jentry);
+        if(ghit<1) continue;
+        for(int i=0;i<ghit;i++) {
+            for(int j=0;j<ghit;j++) {
+                if(i==j) continue;
+                if(abs(gt[i]-gt[j])>dgt ) continue;
+                if(ge[i]<20 || ge[i]>MaxE) continue;
+                if(ge[j]<20 || ge[j]>MaxE) continue;
+                h2x->Fill(ge[i]);
+                h2xy->Fill(ge[i],ge[j]);    
+            }
+        }
+    }
+    return 1;
+}
